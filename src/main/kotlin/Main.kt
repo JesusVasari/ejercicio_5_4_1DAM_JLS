287c727ae2c5d523a5693b1b8f1c00ff901e1cd2
JesusVasari <jlopez 2rafaelalberti.es>
2022-01-14 19:54:56 +0100

    fun info hay que hacer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,69 +5,80 @@ import org.w3c.dom.NodeList
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 
-// El siguiente fragmento lee el archivo Xml y lo transforma en un documento:
-fun readXml(pathName: String): Document {
-    val xmlFile = File(pathName)
-    return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile)
-}
 
-fun obtenerAtributosEnMapKV(e: Element): MutableMap<String, String> {
-    val mMap = mutableMapOf<String, String>()
-    for (j in 0..e.attributes.length - 1)
-        mMap.putIfAbsent(e.attributes.item(j).nodeName, e.attributes.item(j).nodeValue)
-    return mMap
-}
 
-fun obtenerListaNodosPorNombre(doc: Document, tagName: String): MutableList<Node> {
-    val bookList: NodeList = doc.getElementsByTagName(tagName)
-    val lista = mutableListOf<Node>()
-    for (i in 0..bookList.length - 1)
-        lista.add(bookList.item(i))
-    return lista
-}
+class catalogoLibrosXML {
 
-class catologoLibrosXML {
-
-    fun existeLibro(id: String): Boolean {
-        return true
+    init {
+        val documento = readXml("C:\\xml\\ejercicio")
     }
 
-    fun infoLibro( id: String) {
-        val xlmFile: File = File("C:\\xml\\ejercicio")
-        val xmlDoc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xlmFile)
-        xmlDoc.documentElement.normalize()
-        val bookList: NodeList = xmlDoc.getElementsByTagName("book")
+    fun existeLibro(doc: Document, id: String): MutableList<Node> {
+        val bookList: NodeList = doc.getElementsByTagName(id)
+        val lista = mutableListOf<Node>()
+        for (i in 0..bookList.length - 1)
+            lista.add(bookList.item(i))
+        return lista
+    }
 
-        for (i in 0..bookList.length - 1) {
-            var bookNode: Node = bookList.item(i)
+    private fun obtenerAtributosEnMapKV(e: Element): MutableMap<String, String> {
+        val mMap = mutableMapOf<String, String>()
+        for (j in 0..e.attributes.length - 1)
+            mMap.putIfAbsent(e.attributes.item(j).nodeName, e.attributes.item(j).nodeValue)
 
-            if (bookNode.getNodeType() === Node.ELEMENT_NODE) {
+        return mMap
+    }
 
-                val elem = bookNode as Element
-                val mMap = mutableMapOf<String, String>()
-                for (j in 0..elem.attributes.length - 1) {
-                    mMap.putIfAbsent(elem.attributes.item(j).nodeName, elem.attributes.item(j).nodeValue)
-                }
-                println("Current Book : ${bookNode.nodeName} - $mMap")
+    fun obtenerListaNodosPorNombre(doc: Document, tagName: String): MutableList<Node> {
+        val bookList: NodeList = doc.getElementsByTagName(tagName)
+        val lista = mutableListOf<Node>()
+        for (i in 0..bookList.length - 1)
+            lista.add(bookList.item(i))
+        return lista
+    }
 
-                println("Author: ${elem.getElementsByTagName("author").item(0).textContent}")
-                println("Title: ${elem.getElementsByTagName("title").item(0).textContent}")
-                println("Genre: ${elem.getElementsByTagName("genre").item(0).textContent}")
-                println("Price: ${elem.getElementsByTagName("price").item(0).textContent}")
-                println("publish_date: ${elem.getElementsByTagName("publish_date").item(0).textContent}")
-                println("description: ${elem.getElementsByTagName("description").item(0).textContent}")
+    fun listadoNombre() {
+        val ruta = readXml("C:\\xml\\ejercicio")
+        val lista = obtenerListaNodosPorNombre(ruta, "book")
+        lista.forEach {
+            if (it.getNodeType() === Node.ELEMENT_NODE) {
+                val elem = it as Element
+                val mMap = obtenerAtributosEnMapKV(it)
+                println("`- Título: ${it.getElementsByTagName("title").item(0).textContent}")
+
             }
         }
+    }
+
+    private fun readXml(pathName: String): Document {
+        val xmlFile = File(pathName)
+        return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile)
+    }
+
+    fun infoLibro(id: String) {
+
+        val ruta = readXml("C:\\xml\\ejercicio")
+
+        val lista = obtenerListaNodosPorNombre(ruta, "book")
+
+        lista.forEach {
+            if (it.getNodeType() === Node.ELEMENT_NODE) {
+                val elem = it as Element
+                val mMap = obtenerAtributosEnMapKV(it)
 
 
+            }
+        }
     }
 }
-
 
 
 fun main() {
 
     var info: catologoLibrosXML = catologoLibrosXML()
-    info.infoLibro("bk102")
+   info.listadoNombre()
+
+
+
 
 }
